a8eb099d2f20a8ed791fcb7bd361f71eea991424
Amirreza Ghaffari Daryan <71985465+amirreza-ghaffari@users.noreply.github.com>
2023-05-09 17:06:53 +0330

    Update and rename subarray_sort to subarray_sort.py

--- subarray_sort.py
+++ subarray_sort.py
@@ -1,5 +1,5 @@
-Write a function that takes in an array of integers of length at least 2. The function should return an array of the starting and ending indices of the smallest 
-subarray in the input array that needs to be sorted in place in order for the entire input array to be sorted. If the input array is already sorted, the function should return [-1, -1].
+#Write a function that takes in an array of integers of length at least 2. The function should return an array of the starting and ending indices of the smallest 
+#subarray in the input array that needs to be sorted in place in order for the entire input array to be sorted. If the input array is already sorted, the function should return [-1, -1].
 
 
 # Time: O(N)
